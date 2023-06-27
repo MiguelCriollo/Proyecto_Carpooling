13094d4384ee8b3937a779f81e4457d3c94a3319
Dav P <pozodavid060@gmail.com>
2023-06-27 18:01:38 -0500

    loading page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingPageComponent } from './loading-page/loading-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingPageComponent
   ],
   imports: [
     BrowserModule,
