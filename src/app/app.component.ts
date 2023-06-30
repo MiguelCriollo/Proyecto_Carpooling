d6c9b3dd5fa40697a60d80db627fed4ee60b4f07
Dav P <pozodavid060@gmail.com>
2023-06-29 21:55:40 -0500

    sign in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'Proyecto_Carpooling';
-  isLoading: boolean = false;
+  isLoading: boolean = true;
 
   ngOnInit(){
-
+    this.isLoading = false;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoadingPageComponent } from './loading-page/loading-page.component';
+import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
+import { LogotypeComponent } from './logo/logotype/logotype.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+import { SubmitButtonDirective } from './directives/submit-button/submit-button.directive';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingPageComponent
+    LoadingPageComponent,
+    LogotypeComponent,
+    LoginPageComponent,
+    SubmitButtonDirective,
+    SignInPageComponent
   ],
   imports: [
     BrowserModule,
