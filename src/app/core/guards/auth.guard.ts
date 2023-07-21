36dfbe4925254a0d562b679b178aa1c630573e3b
Dav P <pozodavid060@gmail.com>
2023-07-21 13:39:18 -0500

    adjust

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,7 +2,8 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { UserAuthService } from '../services/user-auth.service';
 
-export const AuthGuard: CanActivateFn = () => {
+export const AuthGuard: CanActivateFn = (component: unknown) => {
   let user = inject(UserAuthService);
+  console.log(component);
   return user.isUserAuth();
 };
