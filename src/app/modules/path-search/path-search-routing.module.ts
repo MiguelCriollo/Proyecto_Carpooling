20454c2d596a02c67a0a2c949f67558534223967
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-08-05 13:55:17 -0500

    cargar minContainer

--- src/app/modules/path-search/path-search-routing.module.ts
+++ src/app/modules/path-search/path-search-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainContainerComponent } from './main-container/main-container.component';
+const routes: Routes = [
+  { path: '', component: MainContainerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
